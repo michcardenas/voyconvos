53cdcf3211f0f5eb194227fa5b06a98c5cbed240
michael.cardenas <michael.cardenas@realtechltda.com>
2025-06-22 16:22:17 -0500

    up

--- resources/views/pasajero/viajesDisponibles.blade.php
+++ resources/views/pasajero/viajesDisponibles.blade.php
@@ -857,17 +857,17 @@
 
                                     <!-- Driver -->
                                     <div class="detail-row driver-row">
-                                   <div class="driver-avatar">
-                            @if($viaje->conductor)
-                                <img src="{{ $viaje->conductor->foto ? asset('storage/' . $viaje->conductor->foto) : asset('img/usuario.png') }}" 
-                                    alt="{{ $viaje->conductor->name }}" 
-                                    class="driver-photo">
-                            @else
-                                <div class="driver-photo-placeholder">
-                                    <i class="fas fa-user"></i>
-                                </div>
-                            @endif
-                        </div>
+                               <div class="driver-avatar">
+    @if($viaje->conductor)
+        <img src="{{ $viaje->conductor->foto ? asset('storage/' . $viaje->conductor->foto) : asset('img/usuario.png') }}" 
+             alt="{{ $viaje->conductor->name }}" 
+             class="driver-photo">
+    @else
+        <div class="driver-photo-placeholder">
+            <i class="fas fa-user"></i>
+        </div>
+    @endif
+</div>
                                                                 <div class="detail-content">
                                             <div class="detail-label">Conductor</div>
                                             <div class="detail-value">
